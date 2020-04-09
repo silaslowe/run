f03ad86660881ef83042d2798f2140dbc6147b6b
Silas Lowe <60553895+silaslowe@users.noreply.github.com>
2020-04-09 09:05:35 -0500

    Add files via upload

---
+++ run.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    $("#submit").click(function(){
+        // let min = $("#minutes").val())
+        // $("#output").text($(min);
+      console.log("hmmmm");
+    })
+  });
+  
+  // $("#submit").click(function(){    
+  // pace = ( $("#minutes").val(), $("#seconds").val(), $("#distance".val()) => {
+  //   let pacer = "";
+  //   paceMin = (min/dist)
+  //   paceSec = Math.round(sec/dist)
+  //   pacer = `${paceMin}.${paceSec}`
+  //   $("#output").text(pacer)
+  // } 
+  // })
